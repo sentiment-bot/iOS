ce1351ecab4d80734f45b47191d29c000d716d8b
Moin Uddin <U_Moin75@yahoo.com>
2019-05-04 19:55:44 -0400

    Remove Web Folders

--- client/src/reducers/index.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from "redux";
-import { userReducer as users } from "./userReducer";
-import { teamReducer as teams } from "./teamReducer";
-
-export default combineReducers({
-    users, teams
-});
